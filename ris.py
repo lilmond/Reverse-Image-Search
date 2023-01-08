a42b7949fbd9c31eac5e8a0197f42252a62e12db
Jan Killian <jan.killian@gmail.com>
2023-01-08 13:18:06 +0100

    Update search engines in code
    - *Google*: add [required](https://webapps.stackexchange.com/questions/167766/reverse-google-image-search-via-url-after-2022-switch-to-google-lens) `client=app` parameter after 2022 switch to Google Lens
    - *Yandex*: remove unnecessary `source=collections` parameter
    - *Bing*: remove unnecessary `FORM=IRSBIQ` parameter
    
    README update follows in next PR (sending from GitHub web UI)

--- ris.py
+++ ris.py
@@ -16,10 +16,10 @@ def main():
         while True:
             image_url = input("Image URL: ")
             results = {}
-            results["Google"] = f"https://www.google.com/searchbyimage?image_url={image_url}"
-            results["Yandex"] = f"https://yandex.com/images/search?source=collections&url={image_url}&rpt=imageview"
-            results["TinEye"] = f"https://www.tineye.com/search/?&url={image_url}"
-            results["Bing"] = f"https://www.bing.com/images/searchbyimage?FORM=IRSBIQ&cbir=sbi&imgurl={image_url}"
+            results["Google"] = f"https://www.google.com/searchbyimage?client=app&image_url={image_url}"
+            results["Yandex"] = f"https://yandex.com/images/search?rpt=imageview&url={image_url}"
+            results["TinEye"] = f"https://tineye.com/search?url={image_url}"
+            results["Bing"] = f"https://www.bing.com/images/searchbyimage?cbir=sbi&imgurl={image_url}"
             for result in results:
                 print(f"{C_BLUE}{result}: {C_RESET}{results[result]}")
             print(f"\r\n")
