e8dd62b3c51dd1a79836f645cb9010a01dcb6e64
lilmond <54730301+lilmond@users.noreply.github.com>
2023-02-21 23:30:44 +0800

    Update ris.py

--- ris.py
+++ ris.py
@@ -1,30 +1,23 @@
-import sys
 import os
-
-C_BLUE = "\u001b[34;1m"
-C_RESET = "\u001b[0;0m"
-
-def clearConsole():
-    if sys.platform == "linux":
-        os.system("clear")
-    elif sys.platform == "win32":
-        os.system("cls")
 
 def main():
     try:
-        clearConsole()
-        while True:
-            image_url = input("Image URL: ")
-            results = {}
-            results["Google"] = f"https://www.google.com/searchbyimage?client=app&image_url={image_url}"
-            results["Yandex"] = f"https://yandex.com/images/search?rpt=imageview&url={image_url}"
-            results["TinEye"] = f"https://tineye.com/search?url={image_url}"
-            results["Bing"] = f"https://www.bing.com/images/searchbyimage?cbir=sbi&imgurl={image_url}"
-            for result in results:
-                print(f"{C_BLUE}{result}: {C_RESET}{results[result]}")
-            print(f"\r\n")
-    except KeyboardInterrupt:
-        sys.exit()
+        c_blue = "\u001b[34;1m"
+        c_reset = "\u001b[0;0m"
 
+        os.system("")
+
+        while True:
+            image_url = input(f"{c_blue}Image URL:{c_reset} ").strip()
+
+            print(
+                f"{c_blue}Google:{c_reset} https://lens.google.com/uploadbyurl?url={image_url}\n" \
+                f"{c_blue}Yandex:{c_reset} https://yandex.com/images/search?rpt=imageview&url={image_url}\n" \
+                f"{c_blue}TinEye:{c_reset} https://tineye.com/search?url={image_url}\n" \
+                f"{c_blue}Bing:{c_reset} https://www.bing.com/images/searchbyimage?cbir=sbi&imgurl={image_url}\n\n"
+            )
+    except KeyboardInterrupt:
+        return
+        
 if __name__ == "__main__":
     main()
